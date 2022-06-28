f15204d55fa5eebe3c8207834c17037f6a71a103
Vladimir <mesvladimir@yandex.ru>
2022-06-29 02:54:09 +0300

    Закончил слайдер

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,11 @@
 let burger = document.querySelector('.toggle');
 let nav = document.querySelector('.navigation');
 let pageHeader = document.querySelector('.page-header');
+let promo = document.querySelector('.promo');
+let btnnext = document.querySelector('.promo__button--next');
+let btnprev = document.querySelector('.promo__button--prew');
+let slideItem = document.querySelectorAll('.slider');
+let i = 0;
 
 burger.addEventListener('click', function(event) {
 
@@ -10,8 +15,33 @@ burger.addEventListener('click', function(event) {
 
 nav.classList.remove('navigation--nojs');
 pageHeader.classList.remove('page-header--nojs');
+promo.classList.remove('promo--nojs');
 
+btnnext.addEventListener('click', function (event) {
+    if (slideItem[i].classList.contains('slider--active')) {
+        slideItem[i].classList.remove('slider--active');
+        i++;
+    if (i >= slideItem.length) {
+        i = 0;
+        slideItem[i].classList.add('slider--active');
+    } else if (i <= slideItem.length) {
+        slideItem[i].classList.add('slider--active');
+        };
+    };
+});
 
+btnprev.addEventListener('click', function (event) {
+    if (slideItem[i].classList.contains('slider--active')) {
+        slideItem[i].classList.remove('slider--active');
+        i--;
+    if (i < 0) {
+        i = slideItem.length-1;
+        slideItem[i].classList.add('slider--active');
+    } else if (i <= slideItem.length) {
+        slideItem[i].classList.add('slider--active');
+        };
+    };
+});
 
 // const navMain = document.querySelector('.main-nav');
 // const navToggle = document.querySelector('.main-nav__toggle');
